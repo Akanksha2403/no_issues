bd12de1495068854c9b91955fa87043c0aeeac84
Akanksha <lit2020026@iiitl.ac.in>
2023-02-28 14:10:59 +0530

    ensuring commit

--- grievance/settings.py
+++ grievance/settings.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
 
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
-    "allauth.account.auth_backends.AuthenticationBackend",
+    'allauth.account.auth_backends.AuthenticationBackend',
 )
 SITE_ID = 1
 MIDDLEWARE = [
