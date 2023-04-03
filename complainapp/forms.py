971677ace43cb61309a016929ef9f436fbb3159c
Prabhav Agrawal <prabhavagrawal7@gmail.com>
2023-04-04 00:02:35 +0530

    login form signup form

--- complainapp/forms.py
+++ complainapp/forms.py
@@ -1,6 +1,44 @@
 from django import forms
+from .models import *
+from django import forms
 from django.utils.timezone import now, timedelta
-from .models import Complain
+
+class SignupForm(forms.Form):
+    first_name = forms.CharField(max_length=100, required=True, label="First Name", widget=forms.TextInput(
+        attrs={'placeholder': 'First Name', 'class': 'form-control'}))
+    last_name = forms.CharField(max_length=100, required=True, label="Last Name", widget=forms.TextInput(
+        attrs={'placeholder': 'Last Name', 'class': 'form-control'}))
+    email = forms.EmailField(max_length=100, required=True, label="Email", widget=forms.TextInput(
+        attrs={'placeholder': 'someone@iiitl.ac.in', 'class': 'form-control'}))
+    pass1 = forms.CharField(max_length=100, required=True, label="Enter Password", widget=forms.PasswordInput(
+        attrs={'placeholder': '', 'class': 'form-control'}))
+    pass2 = forms.CharField(max_length=100, required=True, label="Enter Your Password again", widget=forms.PasswordInput(
+        attrs={'placeholder': '', 'class': 'form-control'}))
+
+
+class LoginForm(forms.Form):
+    email = forms.EmailField(max_length=100, required=True, widget=forms.TextInput(
+        attrs={'placeholder': 'Email', 'label': 'Email', 'class': 'form-control'}))
+    password = forms.CharField(max_length=100, required=True, widget=forms.PasswordInput(
+        attrs={'placeholder': 'Password', 'label': 'Password', 'class': 'form-control'}))
+
+
+"""
+class Complain(models.Model):
+    id = models.AutoField
+    heading = models.CharField(max_length=300)
+    description = models.TextField()
+    registered_by = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    registered_to = models.ForeignKey('Designation', on_delete=models.CASCADE)
+    registered_date = models.DateField(default=now, db_index=True)
+    response_date = models.DateField(
+        default=datetime.utcnow() + timedelta(days=1))
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.heading + " to " + self.registered_to.name
+
+"""
 
 class ComplainForm(forms.ModelForm):
     class Meta:
