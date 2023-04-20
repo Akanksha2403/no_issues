e544b87048421b5f02502dde8ac3975d2b3703df
Prabhav Agrawal <prabhavagrawal7@gmail.com>
2023-04-20 19:16:52 +0530

    nill changes

--- complainapp/tests.py
+++ complainapp/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+"""
+git clone - only one time
+git pull 
+git add .
+git commit -m "changes ke bare me"
+git push
+"""
+
+"""
+git branch other - create new branch
+git checkout other - switch to other branch
+// yahan pe apna fir koi bhi kaam kar sakte ho
+git add .
+git commit -m "changes ke bare me"
+git push
+git checkout master - switch to master branch
+git pull - get latest changes
+git merge other - merge other branch to master
+git push
+
+"""
